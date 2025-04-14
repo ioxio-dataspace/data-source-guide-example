88de3f40f0a7d5440ced7f168f66507f26d769b6
Andrew <dyusha.ru@gmail.com>
2025-04-14 17:56:49 +0800

    update runtime entrypoint

--- docker/runtime-entrypoint.sh
+++ docker/runtime-entrypoint.sh
@@ -6,9 +6,6 @@
 # shellcheck disable=SC2039
 set -exuo pipefail
 
-# Works even when mounted on Windows
-export INVOKE_RUN_SHELL="/bin/sh"
-
 # Using PORT if defined (in Google Cloud Run), defaulting to 8000
 export PORT=${PORT:-8000}
 
